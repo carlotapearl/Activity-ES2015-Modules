e0651dfb414c1dc1f1a7efdc841cd9d76b5518a2
Carlota Pearl <email@carlotapearl.com>
2018-05-03 14:55:12 -0400

    Add files via upload

---
+++ utils/arrays.js
@@ -0,0 +1,7 @@
+/*
+INSTRUCTIONS
+1- The only thing that should be in app.js are all of the appropriate imports and all the function calls.
+2 - For each category, create a corresponding file. i.e, Strings = strings.js
+3 - Put each of the files created in a /utils directory
+4 - Each category will have export / import instructions. Read carefully!
+*/
